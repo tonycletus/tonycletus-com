9ff42f315cbf330e36bde19fa727eb49a1920145
tonycletus <anthonyomojocletus@gmail.com>
2023-01-03 17:50:49 +0100

    fixed icon

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", e => {
     e.waitUntil(
-        caches.open("static3")
+        caches.open("static4")
         .then(cache => {
             return cache.addAll([
                 './',
@@ -8,7 +8,6 @@ self.addEventListener("install", e => {
                 './posts/estam-digital-marketing.html',
                 './westsunset.html',
                 './css/style.css',
-                './boxicons/css/boxicons.min.css',
                 './img/logo.png'
                 
 
