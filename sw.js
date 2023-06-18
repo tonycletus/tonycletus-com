5530071d76bab97ba96439dac1f02717229c3ea9
tonycletus <anthonyomojocletus@gmail.com>
2023-06-18 20:29:42 +0100

    improved resposiveness

--- sw.js
+++ sw.js
@@ -1,26 +1,42 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static6")
-        .then(cache => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './posts/estam-digital-marketing.html',
-                './westsunset.html',
-                './css/style.css',
-                './img/logo.png'
-            ])
-        })
-    )
-    console.log("Installed!");
-})
+const CACHE_NAME = "static-v4";
+const urlsToCache = [
+  './',
+  './index.html',
+  './posts/estam-digital-marketing.html',
+  './westsunset.html',
+  './css/style.css',
+  './img/logo.png'
+];
 
-self.addEventListener("fetch", e => { 
-    e.respondWith(
-        caches.match(e.request)
-        .then(response => {
-            return response || fetch(e.request);
-        })
-    )
-    //console.log(`Intercepting fetch for ${e.request.url}`);
-})
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
